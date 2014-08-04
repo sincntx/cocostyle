8cf116293a05eca097c547bef63982e9a6dcbbb8
Park Hyun Chen <sincntx@gmail.com>
2014-08-04 09:24:06 +0900

    Create csToast.js

---
+++ src/csToast.js
@@ -0,0 +1,153 @@
+var csToast = cc.Layer.extend({
+    _duration:2,
+    _labelChild:null,
+    _backgroundChild:null,
+    _backgroundImage:null,
+    _backgroundImageCheck:false,
+    ctor:function() {
+        var winSize = cc.director.getWinSize();
+
+        this._super();
+
+        _backgroundChild = cc.LayerColor.create(cc.color(50, 50, 50, 255));
+        _backgroundChild.setLocalZOrder(1);
+        _labelChild = cc.LabelTTF.create("", "Arial", 15);
+        _labelChild.setLocalZOrder(3);
+        _backgroundImageCheck = false;
+
+        this.adjustBackgroundChild();
+
+        this.addChild(_backgroundChild);
+        this.addChild(_labelChild);
+
+        this.setPosition(winSize.width / 2, winSize.height / 2);
+    },
+    set:function(params) {
+        if(params.backgroundColor) {
+            this.setBackgroundColor(params.backgroundColor);
+        }
+
+        if(params.fontFillColor) {
+            this.setFontFillColor(params.fontFillColor);
+        }
+
+        if(params.fontSize) {
+            this.setFontSize(params.fontSize);
+        }
+
+        if(params.fontName) {
+            this.setFontName(params.fontName);
+        }
+
+        if(params.text) {
+            this.setText(params.text);
+        }
+
+        if(params.duration) {
+            this.setDuration(params.duration);
+        }
+
+        if(params.position) {
+            this.setPosition(params.position);
+        }
+
+        if(params.backgroundImage) {
+            this.setBackgroundImage(params.backgroundImage);
+        }
+    },
+    adjustBackgroundChild:function() {
+        var winSize = cc.director.getWinSize();
+
+        _backgroundChild.width = _labelChild.getContentSize().width + (winSize.width / 20);
+        _backgroundChild.height = _labelChild.getContentSize().height + (winSize.height / 20);
+        _backgroundChild.setPosition(cc.p(_backgroundChild.width / -2, _backgroundChild.height / -2));
+
+        if(_backgroundImageCheck) {
+            _backgroundImage.setScaleX((_labelChild.getContentSize().width + (winSize.width / 20)) / _backgroundImage.getContentSize().width);
+            _backgroundImage.setScaleY((_labelChild.getContentSize().height + + (winSize.height / 20)) / _backgroundImage.getContentSize().height);
+        }
+    },
+    setBackgroundImage:function(backgroundImage) {
+        var winSize = cc.director.getWinSize();
+
+        if(_backgroundImageCheck) {
+            var sprite = cc.Sprite.create(backgroundImage);
+
+            _backgroundImage.setTexture(sprite.texture);
+            _backgroundImage.setScaleX((_labelChild.getContentSize().width + (winSize.width / 20)) / _backgroundImage.getContentSize().width);
+            _backgroundImage.setScaleY((_labelChild.getContentSize().height + + (winSize.height / 20)) / _backgroundImage.getContentSize().height);
+        }
+        else {
+            _backgroundImage = cc.Sprite.create(backgroundImage);
+            _backgroundImage.setScaleX((_labelChild.getContentSize().width + (winSize.width / 20)) / _backgroundImage.getContentSize().width);
+            _backgroundImage.setScaleY((_labelChild.getContentSize().height + + (winSize.height / 20)) / _backgroundImage.getContentSize().height);
+            _backgroundImage.setLocalZOrder(2);
+            this.addChild(_backgroundImage);
+            _backgroundImageCheck = true;
+        }
+    },
+    getBackgroundImage:function() {
+        return _backgroundImage;
+    },
+    setBackgroundColor:function(backgroundColor) {
+        _backgroundChild.setColor(backgroundColor);
+    },
+    getBackgroundColor:function() {
+        return _backgroundChild.getColor();
+    },
+    setFontFillColor:function(fontFillColor) {
+        _labelChild.setFontFillColor(fontFillColor);
+    },
+    getFontFillColor:function() {
+        return _labelChild.getColor();
+    },
+    setFontName:function(fontName) {
+        _labelChild.setFontName(fontName);
+    },
+    getFontName:function() {
+        return _labelChild.getFontName();
+    },
+    setFontSize:function(fontSize) {
+        _labelChild.setFontSize(fontSize);
+        this.adjustBackgroundChild();
+    },
+    getFontSize:function() {
+        return _labelChild.getFontSize();
+    },
+    setText:function(text) {
+        _labelChild.setString(text);
+        this.adjustBackgroundChild();
+    },
+    getText:function() {
+        return _labelChild.getString();
+    },
+    setDuration:function(duration) {
+        _duration = duration;
+    },
+    getDuration:function() {
+        return _duration;
+    },
+    run:function(parent) {
+        var childFadeInAction = cc.FadeIn.create(0.5);
+        var childFadeOutAction = cc.FadeOut.create(0.5);
+        var childDelayAction = cc.DelayTime.create(this._duration);
+        var toastDelayAction = cc.DelayTime.create(this._duration + 1);
+        var toastFinishAction = cc.CallFunc.create(function() {
+            this.removeFromParent(true);
+        }, this);
+        var childSequenceAction = cc.Sequence.create(childFadeInAction, childDelayAction, childFadeOutAction);
+        var toastSequenceAction = cc.Sequence.create(toastDelayAction, toastFinishAction);
+
+        _labelChild.setOpacity(0);
+        _labelChild.runAction(childSequenceAction.clone());
+        _backgroundChild.setOpacity(0);
+        _backgroundChild.runAction(childSequenceAction.clone());
+        if(_backgroundImageCheck) {
+            _backgroundImage.setOpacity(0);
+            _backgroundImage.runAction(childSequenceAction.clone());
+        }
+
+        parent.addChild(this);
+        this.runAction(toastSequenceAction);
+    }
+});
