f6be751f5e63629ef10b70e5d2a0e0ab33a02d1d
Park Hyun Chen <sincntx@gmail.com>
2014-08-11 17:14:47 +0900

    Update csCryptoTest.js
    add AES

--- test/csCryptoTest.js
+++ test/csCryptoTest.js
@@ -26,16 +26,24 @@ var csCryptoTestLayer = cc.Layer.extend({
         this.addChild(menu);
 
         var tlabel = cc.LabelTTF.create(csCrypto.md5("name"), "Arial", 30);
-        tlabel.setPosition(winSize.width / 2, winSize.height / 2 + 100);
+        tlabel.setPosition(winSize.width / 2, winSize.height / 2 + 150);
         this.addChild(tlabel);
 
         var tlabel2 = cc.LabelTTF.create(csCrypto.sha1("name"), "Arial", 30);
-        tlabel2.setPosition(winSize.width / 2, winSize.height / 2);
+        tlabel2.setPosition(winSize.width / 2, winSize.height / 2 + 75);
         this.addChild(tlabel2);
 
         var tlabel3 = cc.LabelTTF.create(csCrypto.sha256('hex', "name"), "Arial", 30);
-        tlabel3.setPosition(winSize.width / 2, winSize.height / 2 - 100);
+        tlabel3.setPosition(winSize.width / 2, winSize.height / 2);
         this.addChild(tlabel3);
+
+        var aes        = new AES({block_mode:CBC, iv: 'asdfasdfasdfasdf', key: convert.hex_to_binstring('0123456712345678234567893456789a0123456712345678234567893456789a')});
+        var data        = "rank2";
+        var encrypted   = convert.binstring_to_hex(aes.encrypt(data));
+
+        var tlabel4 = cc.LabelTTF.create(encrypted, "Arial", 30);
+        tlabel4.setPosition(winSize.width / 2, winSize.height / 2 - 75);
+        this.addChild(tlabel4);
 
         return true;
     }
