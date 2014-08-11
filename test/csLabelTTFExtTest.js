f978a49c8957d59c44f9fa3109b1c8ffb6c555db
Park Hyun Chen <sincntx@gmail.com>
2014-08-11 13:52:39 +0900

    Create csLabelTTFExtTest.js

---
+++ test/csLabelTTFExtTest.js
@@ -0,0 +1,44 @@
+var csLabelTTFExtTestLayer = cc.Layer.extend({
+    ctor:function () {
+        var winSize;
+        var toast;
+        var label, menuItem, menu, title;
+        this._super();
+
+        winSize = cc.director.getWinSize();
+
+        title = cc.LabelTTF.create("Cocostyle Label TTF Ext Test", "Arial", 20);
+        title.setPosition(winSize.width / 2, winSize.height - 40);
+        title.setColor(cc.color(255, 255, 0, 255));
+        this.addChild(title);
+
+        label = cc.LabelTTF.create("[ Main Menu ]", "Arial", 15);
+        menuItem = cc.MenuItemLabel.create(label, function() {
+            var scene = new MainTestLayer();
+            cc.director.runScene(scene);
+        }, this);
+
+        menu = cc.Menu.create(menuItem);
+        menu.x = 0;
+        menu.y = 0;
+        menuItem.x = winSize.width - 70;
+        menuItem.y = 25;
+        this.addChild(menu);
+
+        var tlabel = new csLabelTTFExt("My name is typing label!!!!!", "Arial", 30);
+        tlabel.setPosition(winSize.width / 2, winSize.height / 2);
+        tlabel.setFontStyle("bold italic");
+        this.addChild(tlabel);
+
+        return true;
+    }
+});
+
+var csLabelTTFExtTestScene = cc.Scene.extend({
+    onEnter:function () {
+        this._super();
+        var layer = new csLabelTTFExtTestLayer();
+        this.addChild(layer);
+    }
+});
+
