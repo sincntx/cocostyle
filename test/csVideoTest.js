ce7ef7fbdf28cb43f46c70d220e68f8e224290c1
Park Hyun Chen <sincntx@gmail.com>
2014-10-15 17:34:09 +0900

    Create csVideoTest.js

---
+++ test/csVideoTest.js
@@ -0,0 +1,44 @@
+var csVideoTestLayer = cc.Layer.extend({
+    ctor:function () {
+        var winSize;
+        var label, menuItem, menu, title;
+        this._super();
+
+        winSize = cc.director.getWinSize();
+
+        title = new cc.LabelTTF("Cocostyle Video Test", "Arial", 20);
+        title.setPosition(winSize.width / 2, winSize.height - 40);
+        title.setColor(cc.color(255, 255, 0, 255));
+        this.addChild(title);
+
+        label = new cc.LabelTTF("[ Main Menu ]", "Arial", 15);
+        menuItem = new cc.MenuItemLabel(label, function() {
+            var scene = new MainTestLayer();
+            cc.director.runScene(scene);
+        }, this);
+
+        menu = new cc.Menu(menuItem);
+        menu.x = 0;
+        menu.y = 0;
+        menuItem.x = winSize.width - 70;
+        menuItem.y = 25;
+        this.addChild(menu);
+
+        var video = new csVideo('http://www.w3schools.com/html/mov_bbb.mp4');
+        video.setPosition(cc.p(winSize.width / 2, winSize.height));
+        video.setRotation(100);
+        video.runAction(new cc.ScaleTo(2, 2));
+        video.video.play();
+        this.addChild(video);
+
+        return true;
+    }
+});
+
+var csVideoTestScene = cc.Scene.extend({
+    onEnter:function () {
+        this._super();
+        var layer = new csVideoTestLayer();
+        this.addChild(layer);
+    }
+});
