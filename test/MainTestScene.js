80768c841ccef128ec3dce27d8a19404bf1df4f1
Park Hyun Chen <sincntx@gmail.com>
2014-12-09 20:38:30 +0900

    Update MainTestScene.js

--- test/MainTestScene.js
+++ test/MainTestScene.js
@@ -1,87 +1,64 @@
 var MainTestLayer = cc.Layer.extend({
+    menuItemList : null,
     ctor:function () {
         var winSize;
-        var menu, title;
-        var menuItemList;
+        var title;
+        var menuItemList, i;
+        var btn, label;
 
         this._super();
 
         winSize = cc.director.getWinSize();
 
-        title = new cc.LabelTTF("Cocostyle Test List", "Arial", 20);
-        title.x = winSize.width / 2;
-        title.y = winSize.height - 40;
-        title.color = cc.color(255, 255, 0);
+        title = new cc.Sprite(res.logo_png);
+        title.scale = 0.7;
+        title.x = 150;
+        title.y = winSize.height - 50;
         this.addChild(title);
 
-        menuItemList = [];
+        this.menuItemList = [{title:'csButton',desc:'a simple and beautiful button.'},
+            {title:'csToast',scene:'csToast',desc:'a quick little message.'},
+            {title:'csAlert',scene:'csAlet',desc:'a simple alert window.'},
+            {title:'csTypingLabel',scene:'csTypingLabel',desc:'a label with typing effect.'},
+            {title:'csTypingLBExt',scene:'csTypingLabelExt',desc:'a label with typing effect and font style.'},
+            {title:'csLabelTTFExt',scene:'csLabelTTFExt',desc:'a label with font style(bold, italic and etc).'},
+            {title:'csCrypto',scene:'csCrypto',desc:'MD5, SHA1, SHA256, AES and etc.'},
+            {title:'csVideo',scene:'csVideo',desc:'a video player as cocos2d sprite.'},
+            {title:'csIframe',scene:'csIframe',desc:'a iFrame as cocos2d sprite.'},
+            {title:'csAntiAlising',scene:'csCanvasAntiAlising',desc:'anti alising on canvas.'},
+            {title:'csValidator',scene:'csValidator',desc:'validate email, url, credit card number and etc.'},
+            {title:'csMoment',scene:'csMoment',desc:'parse, validate, manipulate, and display dates.'}
+        ];
 
-        menuItemList.push(new cc.MenuItemLabel(new cc.LabelTTF("[ csToast ]", "Arial", 15), function() {
-            var scene = new csToastTestLayer();
-            cc.director.runScene(scene);
-        }, this));
+        var scrollView = new ccui.ScrollView();
+        scrollView.setDirection(ccui.ScrollView.DIR_VERTICAL);
+        scrollView.setTouchEnabled(true);
+        scrollView.setContentSize(cc.size(winSize.width, winSize.height));
+        var innerWidth = scrollView.width;
+        var innerHeight = this.menuItemList.length * 150;
+        scrollView.setInnerContainerSize(cc.size(innerWidth, innerHeight));
+        this.addChild(scrollView);
 
-        menuItemList.push(new cc.MenuItemLabel(new cc.LabelTTF("[ csPixelCollision ]", "Arial", 15), function() {
-            var scene = new csPixelCollisionTestLayer();
-            cc.director.runScene(scene);
-        }, this));
+        for(i = 0;i < this.menuItemList.length;i++) {
+            var btn = new csButton(this.menuItemList[i].title, csButton.style.emptycircle, csButton.size.large, csButton.color.orange, function(sender) {
+                var scene = eval('new ' + sender.parent.scene + 'TestLayer()');
+                cc.director.runScene(scene);
+            }, btn);
 
-        menuItemList.push(new cc.MenuItemLabel(new cc.LabelTTF("[ csTypingLabel ]", "Arial", 15), function() {
-            var scene = new csTypingLabelTestLayer();
-            cc.director.runScene(scene);
-        }, this));
+            btn.scene = this.menuItemList[i].scene;
+            btn.labelItem.label.fontSize = 10;
+            btn.labelItem.width = btn.labelItem.label.width;
+            btn.labelItem.height = btn.labelItem.label.height;
+            btn.x = winSize.width /  2 - 60;
+            btn.y = (this.menuItemList.length * 150 - 100) - (i * 150);
+            scrollView.addChild(btn);
 
-        menuItemList.push(new cc.MenuItemLabel(new cc.LabelTTF("[ csTypingLabelExt ]", "Arial", 15), function() {
-            var scene = new csTypingLabelExtTestLayer();
-            cc.director.runScene(scene);
-        }, this));
-
-        menuItemList.push(new cc.MenuItemLabel(new cc.LabelTTF("[ csLabelTTFExt ]", "Arial", 15), function() {
-            var scene = new csLabelTTFExtTestLayer();
-            cc.director.runScene(scene);
-        }, this));
-
-        menuItemList.push(new cc.MenuItemLabel(new cc.LabelTTF("[ csCrypto ]", "Arial", 15), function() {
-            var scene = new csCryptoTestLayer();
-            cc.director.runScene(scene);
-        }, this));
-
-        menuItemList.push(new cc.MenuItemLabel(new cc.LabelTTF("[ csAlert ]", "Arial", 15), function() {
-            var scene = new csAlertTestLayer();
-            cc.director.runScene(scene);
-        }, this));
-
-        menuItemList.push(new cc.MenuItemLabel(new cc.LabelTTF("[ csVideo ]", "Arial", 15), function() {
-            var scene = new csVideoTestLayer();
-            cc.director.runScene(scene);
-        }, this));
-
-        menuItemList.push(new cc.MenuItemLabel(new cc.LabelTTF("[ csIframe ]", "Arial", 15), function() {
-            var scene = new csIframeTestLayer();
-            cc.director.runScene(scene);
-        }, this));
-
-        menuItemList.push(new cc.MenuItemLabel(new cc.LabelTTF("[ csCanvasAntiAlising ]", "Arial", 15), function() {
-            var scene = new csCanvasAntiAlisingTestLayer();
-            cc.director.runScene(scene);
-        }, this));
-
-        menuItemList.push(new cc.MenuItemLabel(new cc.LabelTTF("[ csValidator ]", "Arial", 15), function() {
-            var scene = new csValidatorTestLayer();
-            cc.director.runScene(scene);
-        }, this));
-
-        menuItemList.push(new cc.MenuItemLabel(new cc.LabelTTF("[ csMoment ]", "Arial", 15), function() {
-            var scene = new csMomentTestLayer();
-            cc.director.runScene(scene);
-        }, this));
-
-        menu = new cc.Menu(menuItemList);
-        menu.alignItemsVerticallyWithPadding(10);
-        menu.x = winSize.width / 2;
-        menu.y = winSize.height / 2;
-
-        this.addChild(menu);
+            label = new cc.LabelTTF(this.menuItemList[i].desc, "Arial", 10);
+            label.x = winSize.width / 2;
+            label.y = (this.menuItemList.length * 150 - 100) - (i * 150);
+            label.anchorX = 0;
+            scrollView.addChild(label);
+        }
 
         return true;
     }
