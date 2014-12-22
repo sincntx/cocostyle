c567ecb4871f19ce1710db0ca2dc4e83b966ad05
Park Hyun Chen <sincntx@gmail.com>
2014-12-22 20:31:51 +0900

    Update csTypingLabel.js

--- src/csTypingLabel.js
+++ src/csTypingLabel.js
@@ -4,45 +4,8 @@ var csTypingLabel = cc.LabelTTF.extend({
     _nowCur:0,
     ctor: function (text, fontName, fontSize, dimensions, hAlignment, vAlignment) {
         cc.LabelTTF.prototype.ctor.call(this);
-
-        this._dimensions = cc.size(0, 0);
-        this._hAlignment = cc.TEXT_ALIGNMENT_LEFT;
-        this._vAlignment = cc.VERTICAL_TEXT_ALIGNMENT_TOP;
-        this._opacityModifyRGB = false;
-        this._fontStyleStr = "";
-        this._fontName = "Arial";
-        this._isMultiLine = false;
-
-        this._shadowEnabled = false;
-        this._shadowOffset = cc.p(0, 0);
-        this._shadowOpacity = 0;
-        this._shadowBlur = 0;
-        this._shadowColorStr = "rgba(128, 128, 128, 0.5)";
-
-        this._strokeEnabled = false;
-        this._strokeColor = cc.color(255, 255, 255, 255);
-        this._strokeSize = 0;
-        this._strokeColorStr = "";
-
-        this._textFillColor = cc.color(255, 255, 255, 255);
-        this._fillColorStr = "rgba(255,255,255,1)";
-        this._strokeShadowOffsetX = 0;
-        this._strokeShadowOffsetY = 0;
-        this._needUpdateTexture = false;
-
-        this._lineWidths = [];
-
         this._originalString = text;
         text = ' ';
-
-        this._setColorsString();
-
-        if (fontName && fontName instanceof cc.FontDefinition) {
-            this.initWithStringAndTextDefinition(text, fontName);
-        }
-        else {
-            cc.LabelTTF.prototype.initWithString.call(this, text, fontName, fontSize, dimensions, hAlignment, vAlignment);
-        }
     },
     run:function(duration) {
         this.schedule(this.onTyping, duration);
